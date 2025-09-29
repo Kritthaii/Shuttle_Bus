2a9f69909cdba9e6556df4261656564a74d73109
Audwa188 <6611130019@mut.ac.th>
2025-09-29 11:16:49 +0700

    Merge Aud-UserManage-Home into main

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,9 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <div className="h-screen flex items-center justify-center bg-gray-100 ">
-        <h1 className="text-3xl font-bold text-red-950">Hello Tailwind!</h1>
-      </div>
+      <h1>ยินดีต้อนรับ,Admin</h1>
     </div>
   );
 }
